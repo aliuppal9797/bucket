758a8d66daa18273f054ec4fd6ef01f7225dd485
aliuppal9797 <80312742+aliuppal9797@users.noreply.github.com>
2022-04-14 15:49:43 +0500

    PC last updated

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const {
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    getProductDetails,
+    deleteProduct,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+const router = express.Router();
+
+router
+    .route("/products")
+    .get(getAllProducts);
+
+
+router
+    .route("/product/new")
+    .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
+router
+    .route("/product/:id")
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+    .get(getProductDetails);
+
+
+
+
+
+module.exports = router;
